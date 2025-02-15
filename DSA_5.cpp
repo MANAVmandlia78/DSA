6ee8d806718736cd20200c0941e0d44876d0af0d
Manav Mandalia <manavmandalia077@gmail.com>
2025-02-15 19:18:59 +0530

    Add files via upload

---
+++ DSA_5.cpp
@@ -0,0 +1,21 @@
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x) {  data = x;  next = nullptr; }
+}; 
+
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    bool searchKey(int n, Node* head, int key) {
+        Node*temp = head;
+        while(temp!=nullptr){
+            if(temp->data == key){
+                return true;
+            }
+            temp=temp->next;
+        }
+                return false;
+    }
+};
