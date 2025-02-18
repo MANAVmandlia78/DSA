d61962e2d3fe0b687c0883beb3b1e51865920fc2
Manav Mandalia <manavmandalia077@gmail.com>
2025-02-18 13:08:03 +0530

    Add files via upload

---
+++ DSA_9.cpp
@@ -0,0 +1,24 @@
+// code for identifying a loop in a linked list
+//   Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(nullptr) {}
+  };
+ 
+class Solution {
+    public:
+        bool hasCycle(ListNode *head) {
+            if (!head || !head->next) return false; // Handle edge case
+            ListNode* slow = head;
+            ListNode* fast = head;
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            return true;
+            break;
+        }
+    }
+       return false; }
+    };
