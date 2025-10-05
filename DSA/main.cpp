06fc4f49979750df114125a832bdc90100030792
MANAVmandalia78 <manavmandalia077@gmail.com>
2025-10-05 18:43:16 +0530

    Added main.cpp file inside DSA folder

---
+++ DSA/main.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+pair <int,string> arr[] = {{1,"one"},{2,"two"},{3,"three"}};
+cout<< arr[1].first << " " << arr[1].second;
+    return 0;
+}
